e74a2c80e07c38b5fe65093fc376dc193d0fb8cd
thatzme <yusufizika@gmail.com>
2022-05-05 12:10:12 +0100

    udemy

---
+++ Udemy ML/Study-1.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+veriler = pd.read_csv('veriler.csv')
+boy = veriler[['boy']]
+print(boy)
+boykilo = veriler[['boy','kilo']]
+print(boykilo)
+
+
+veriler = pd.read_csv('eksikveriler.csv')
+
+from sklearn.impute import SimpleImputer
+imputer  = SimpleImputer(missing_values=np.nan, strategy="mean")
+
+Yas = veriler.iloc[:,1:4].values
+print(Yas)
+
+imputer = imputer.fit(Yas[:,1:4])
+Yas[:,1:4] = imputer.transform(Yas[:,1:4])
+print(Yas)
+
+
+ulke = veriler.iloc[:,0:1].values
+print(ulke)
+
+from sklearn import preprocessing
+
+le = preprocessing.LabelEncoder()
+
+ulke[:,0] = le.fit_transform(veriler.iloc[:,0])
+print(ulke)
+
+ohe = preprocessing.OneHotEncoder()
+ulke=ohe.fit_transform(ulke).toarray()
+print(ulke)
