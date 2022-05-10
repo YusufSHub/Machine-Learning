a1ce50aac0167dea822ffe49126645757cf59de2
thatzme <yusufizika@gmail.com>
2022-05-10 17:09:03 +0100

    capstone

--- Udemy ML/veribirlestirme.py
+++ Udemy ML/veribirlestirme.py
@@ -84,21 +84,21 @@ print(s)
 s2=pd.concat([s,sonuc3], axis=1)
 print(s2)
 
+print(s2)
+
+from sklearn.model_selection import train_test_split
 
 
+x_train, x_test, y_train, y_test = train_test_split(s,sonuc3, test_size=0.33, random_state=0)
 
 
+from sklearn.preprocessing import StandardScaler
+#standart scaler
+sc=StandardScaler()
 
+X_train = sc.fit_transform(x_train)
 
-
-
-
-
-
-
-
-
-
+X_test = sc.fit_transform(x_test)
 
 
 
